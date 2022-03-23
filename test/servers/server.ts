e998122f163fdb623ae47869c34d11783c812573
qa-swaneet-sahoo <swaneet.sahoo@qaware.de>
2022-03-23 15:52:28 +0100

    Extended NodeJS server capabilities for the testing.

--- test/servers/server.ts
+++ test/servers/server.ts
@@ -1,5 +1,6 @@
 import protobuf from 'protobufjs';
 import http from 'http';
+import Long from 'long';
 
 const PORT = 8080;
 
@@ -10,6 +11,25 @@ const protoResponsePath = 'happyday.HappyDayResponse';
 let ProtobufDefs: protobuf.Root;
 let HappyDayRequestType: protobuf.Type;
 let HappyDayResponseType: protobuf.Type;
+
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const wednesdayDateWeekday = 3;
+
+// these would be usually automatically generated
+interface HappyDayRequest {
+    date: {
+        seconds: Long;
+        nanos: number;
+    };
+    includeReason: boolean;
+}
+
+interface HappyDayResponse {
+    isHappyDay: boolean;
+    reason: string;
+    formattedDate: string;
+    err: string;
+}
 
 interface PathHandler {
     path: string;
@@ -22,20 +42,56 @@ function defineHandlers(): PathHandler[] {
     return [{
         path: '/happy-day/verify',
         reqType: HappyDayRequestType,
-        handler(req) {
-            return Promise.resolve([HappyDayResponseType, {isHappyDay: true, reason: 'Tuesday is a Happy Day!'}]);
+        async handler(req: HappyDayRequest) {
+            let err = '';
+
+            const date = req?.date ?? {seconds: new Long(0, 0), nanos: 0};
+            const seconds = date.seconds;
+            const nanos = date.nanos;
+
+            const epochMillis = seconds.mul(1000).add(Math.floor(nanos / 1000 / 1000));
+
+            const epochMillisNumber = epochMillis.toNumber();
+
+            if (epochMillis.toString() !== epochMillisNumber.toString()) {
+                return [HappyDayResponseType, {err: err + 'Cannot handle number of millis ' + epochMillis + ' as number: ' + epochMillisNumber + '\n'}];
+            }
+
+            const jsDate = new Date(epochMillisNumber);
+
+            const dateWeekday = jsDate.getUTCDay();
+            const formattedWeekday = weekdays[dateWeekday];
+
+            console.log('Weekday is ' + dateWeekday + ', ' + formattedWeekday);
+
+            const isHappyDay = dateWeekday !== wednesdayDateWeekday;
+
+            return [HappyDayResponseType, {
+                isHappyDay,
+                reason: isHappyDay ? (formattedWeekday + ' is a Happy Day! ⭐') : ('Tough luck on ' + formattedWeekday + '... 😕'),
+                formattedDate: jsDate.toUTCString(),
+                err,
+            } as HappyDayResponse];
         }
-    }];
+    },
+        {
+            path: '/echo',
+            reqType: HappyDayRequestType,
+            async handler(reqDecoded: { [p in string]: any }): Promise<[protobuf.Type, { [p in string]: any }]> {
+                return [HappyDayRequestType, reqDecoded];
+            }
+        }
+    ];
 }
 
 function runHttpServer(handlers: PathHandler[]) {
     const requestListener: http.RequestListener = (req, res) => {
-        console.log(req.method + ' ' + req.url);
+        console.log('=========== ' + req.method + ' ' + req.url);
 
         const currentHandler = handlers.find(handler => handler.path == req.url);
 
         if (currentHandler === undefined) {
-            res.writeHead(404);
+            req.statusCode = 404;
             res.end();
             return;
         }
@@ -62,11 +118,16 @@ function runHttpServer(handlers: PathHandler[]) {
                 .then(([responseType, respMessage]) => {
                     console.log('Encoding response: ' + JSON.stringify(respMessage, null, 2));
                     const encodedMsg = responseType.encode(respMessage).finish();
-                    res.writeHead(200);
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/x-protobuf');
                     res.end(encodedMsg);
+                    console.log('=========== 200 OK');
                 })
                 .catch(err => {
-                    console.error('Error during request handling: ' + err);
+                    console.error('Error during request handling: ');
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.end();
                 });
 
             await Promise.allSettled([result]);
