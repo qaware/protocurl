4b8a2e80c32d17cff1c1625a5d0c0a5c2cb97ba9
qa-swaneet-sahoo <93665842+qa-swaneet-sahoo@users.noreply.github.com>
2022-04-12 02:50:02 +0200

    Update 2-release-docker-image.sh

--- release/2-release-docker-image.sh
+++ release/2-release-docker-image.sh
@@ -3,9 +3,8 @@ set -e
 
 source release/source.sh
 
-# Necessary for local development
-# docker buildx rm protocurl-builder || true
-# docker buildx create --use --name protocurl-builder
+docker buildx rm protocurl-builder || true
+docker buildx create --use --name protocurl-builder
 
 docker buildx build \
   --platform linux/amd64,linux/i386,linux/arm64 \
