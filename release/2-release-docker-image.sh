51e89d5d99aeab9f639d9919554138257272b982
qa-swaneet-sahoo <93665842+qa-swaneet-sahoo@users.noreply.github.com>
2022-04-12 02:53:51 +0200

    Update 2-release-docker-image.sh

--- release/2-release-docker-image.sh
+++ release/2-release-docker-image.sh
@@ -6,8 +6,11 @@ source release/source.sh
 docker buildx rm protocurl-builder || true
 docker buildx create --use --name protocurl-builder
 
+# todo. this does not work currently on GitHub Actions
+# ,linux/arm64
+
 docker buildx build \
-  --platform linux/amd64,linux/i386,linux/arm64 \
+  --platform linux/amd64,linux/i386 \
   --build-arg VERSION=$VERSION \
   -t qaware/protocurl:$VERSION -f release/Dockerfile \
   --push .
