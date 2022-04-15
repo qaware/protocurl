7998fee5c1b0a770c7378bbdae731be109dfeeb7
qa-swaneet-sahoo <swaneet.sahoo@qaware.de>
2022-04-15 20:14:31 +0200

    Windows CI tests with adding into PATH.

--- test/suite/windows-tests.ps1
+++ test/suite/windows-tests.ps1
@@ -4,32 +4,42 @@ $pc = $args[0]
 
 Write-Output "========= Running windows specific tests ========="
 
+function Run-Tests
+{
+    param(
+        $ProtocurlExe
+    )
 
-Write-Output "=== Executable is runnable ==="
-&"$pc\bin\protocurl.exe" -h
+    Write-Output "====== Running tests using executable $ProtocurlExe ======"
 
+    Write-Output "=== Executable is runnable ==="
+    &"$ProtocurlExe" -h
 
-Write-Output "=== Base scenario runs. protoc.exe is found and used. protocurl-internal is found and used ==="
-&"$pc\bin\protocurl.exe" -I test\proto `
+    Write-Output "=== Base scenario runs. protoc.exe is found and used. protocurl-internal is found and used ==="
+    &"$ProtocurlExe" -I test\proto `
     -f happyday.proto -i happyday.HappyDayRequest -o happyday.HappyDayResponse `
     -u http://localhost:8080/happy-day/verify -d "includeReason: true"
 
+    Write-Output "=== Using custom protoc and proto lib and global curl ==="
+    if (Test-Path my-protoc) {
+        Remove-Item my-protoc -Recurse -force
+    }
+    mkdir my-protoc
+    mkdir my-protoc\my-bin
+    Copy-Item "$pc\protocurl-internal\bin\protoc.exe" -Destination my-protoc\my-bin\protoc.exe
+    Copy-Item "$pc\protocurl-internal\include" -Destination my-protoc\my-protos -Recurse
+    Copy-Item "test\proto\*" -Destination my-protoc\my-protos -Recurse
 
-Write-Output "=== Using custom protoc and proto lib and global curl ==="
-if (Test-Path my-protoc) {
-    Remove-Item my-protoc -Recurse -force
-}
-mkdir my-protoc
-mkdir my-protoc\my-bin
-Copy-Item "$pc\protocurl-internal\bin\protoc.exe" -Destination my-protoc\my-bin\protoc.exe
-Copy-Item "$pc\protocurl-internal\include" -Destination my-protoc\my-protos -Recurse
-Copy-Item "test\proto\*" -Destination my-protoc\my-protos -Recurse
-ls my-protoc\my-bin
-ls my-protoc\my-protos
-
-&"$pc\bin\protocurl.exe" -v --curl `
+    &"$ProtocurlExe" -v --curl `
     --protoc-path my-protoc\my-bin\protoc -I my-protoc\my-protos `
     -f happyday.proto -i happyday.HappyDayRequest -o happyday.HappyDayResponse `
     -u http://localhost:8080/happy-day/verify -d "includeReason: true"
+}
+
+Run-Tests(".\$pc\bin\protocurl.exe")
+
+Write-Output "Installing protocurl into PATH and re-executing..."
+$env:Path += ";$PWD\$pc\bin"
+Run-Tests("protocurl")
 
 Write-Output "========= Windows Tests successful. ========="
