f6d3829c47630f6399547f69ec30d8b500b4cb5b
GollyTicker <golly.ticker@gmail.com>
2022-04-17 01:56:37 +0200

    Enable version properly set for untagged commits for CI.

--- release/source.sh
+++ release/source.sh
@@ -7,7 +7,13 @@ source ./release/get-latest-dependencies-versions.sh
 export BUILD_ARCH="$(uname -m | sed "s/x86_64/amd64/" | sed "s/x86_32/386/" | sed "s/aarch_64/arm64/")"
 
 if [[ "$VVERSION" == "" ]]; then
-  export VVERSION="$(git tag --points-at HEAD --sort -version:refname | head -n 1)"
+  GIT_TAG="$(git tag --points-at HEAD --sort -version:refname | head -n 1)"
+  if [[ "$GIT_TAG" != "" ]]; then
+    export VVERSION="$GIT_TAG"
+  else
+    PREVIOUS_TAG="$(git for-each-ref --sort='-version:refname' --count 1 --format '%(refname:short)' refs/tags)"
+    export VVERSION="$PREVIOUS_TAG-next"
+  fi
 fi
 
 # any version: e.g. v1.2.3, v23.45.67-dev
@@ -15,14 +21,14 @@ if [[ "$VVERSION" =~ v.*\..*\..* ]]; then
 
   # any snapshot version with a -
   # e.g. v23.45.67-dev but not v1.2.3
-  if  [[ "$VVERSION" =~ v.*\..*\..*[-].* ]]; then
+  if [[ "$VVERSION" =~ v.*\..*\..*[-].* ]]; then
     export SNAPSHOT="true"
   else
     export SNAPSHOT="false"
   fi
 
   export VERSION="${VVERSION#"v"}"
-  echo "Using VERSION=$VERSION, VVERSION=$VVERSION, SNAPSHOT=$SNAPSHOT, BUILD_ARCH=$BUILD_ARCH, PROTO_VERSION=$PROTO_VERSION"
+  echo "Variables: VERSION=$VERSION, VVERSION=$VVERSION, SNAPSHOT=$SNAPSHOT, BUILD_ARCH=$BUILD_ARCH, PROTO_VERSION=$PROTO_VERSION"
 else
   echo "Closest git tag is not a version tag: $VVERSION"
   echo "Could not extract current version from git tags. Please tag accordingly. Or provide one manually"
