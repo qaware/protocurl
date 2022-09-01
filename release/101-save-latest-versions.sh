b88e3e41c677ce3bc80bc1df25348a6afddd4751
GollyTicker <golly.ticker@gmail.com>
2022-09-01 17:27:07 +0200

    Fix versions check to exclude protoCURL self-reference.

--- release/101-save-latest-versions.sh
+++ release/101-save-latest-versions.sh
@@ -7,5 +7,7 @@ source release/0-get-latest-dependencies-versions.sh
 
 OUT="${OUT:-"release/versions.txt"}"
 
-echo "$ALL_VERSIONS" | sed '/^$/d' >"$OUT"
+# ignore empty lines. ignore protoCURL itself.
+echo "$ALL_VERSIONS" | sed '/^$/d' | sed '/protoCURL/d' >"$OUT"
+
 echo "Saved latest versions into $OUT."
