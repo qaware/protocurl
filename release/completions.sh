4dc0392b7dd151611d2bc580fa86b5d5208b059d
Alexander Eimer <alexander.eimer@qaware.de>
2023-10-23 13:28:13 +0200

    fix: remove completions for now

--- release/completions.sh
+++ release/completions.sh
@@ -5,10 +5,6 @@ set -e
 rm -rf completions
 mkdir completions
 
-(
-  cd src
-
-  for sh in bash zsh fish; do
-    go run protocurl.go completion "$sh" >"../completions/protocurl.$sh"
-  done
-)
+for sh in bash zsh fish; do
+  go run -C src protocurl.go completion "$sh" >"completions/protocurl.$sh"
+done
