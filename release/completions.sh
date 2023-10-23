9c3dddf9e4131e5340b63995dfd3c8e8a31f1457
Alexander Eimer <alexander.eimer@qaware.de>
2023-10-23 12:59:54 +0200

    feat: run cd in subshell

--- release/completions.sh
+++ release/completions.sh
@@ -5,8 +5,10 @@ set -e
 rm -rf completions
 mkdir completions
 
-cd src
+(
+  cd src
 
-for sh in bash zsh fish; do
-	go run protocurl.go completion "$sh" >"../completions/protocurl.$sh"
-done
+  for sh in bash zsh fish; do
+    go run protocurl.go completion "$sh" >"../completions/protocurl.$sh"
+  done
+)
