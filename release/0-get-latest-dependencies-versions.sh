59251fe23a0852aa07b8fdbc548971f5f0485ab6
Ayake <golly.ticker@gmail.com>
2025-10-17 18:06:53 +0200

    Fix release version scripts

--- release/0-get-latest-dependencies-versions.sh
+++ release/0-get-latest-dependencies-versions.sh
@@ -22,6 +22,7 @@ retrieveLatestVersion() {
   TYPE="$1" # tag or release
   REPO="$2"
   TAG_FILTER="$3"
+  VERSION_PREFIX="$4"
 
   FILE_FRIENDLY_NAME="$(echo "$REPO" | sed 's#/#.#g')"
 
@@ -29,32 +30,24 @@ retrieveLatestVersion() {
   ETAG_FILE="release/.cache/$FILE_FRIENDLY_NAME.etag"
   ETAG="$(cat "$ETAG_FILE" 2>/dev/null || echo '')"
 
-  if [[ "$TYPE" == "release" ]]; then
-    ENDPOINT="releases?per_page=20"
-    RESPONSE_FILTER="^$TAG_FILTER\$"
-    PATH_TO_TAG=".tag_name"
-    LATEST_VERSION_EXTRACTOR="head"
-  else
-    # Actually, we should only be getting the first 100 tags.
-    # However, currently github returns all tags.
-    # See issue: https://github.com/github/docs/issues/3863
-    ENDPOINT="git/matching-refs/tags?per_page=100"
-    RESPONSE_FILTER="^refs/tags/$TAG_FILTER\$"
-    PATH_TO_TAG=".ref"
-    LATEST_VERSION_EXTRACTOR="tail"
-  fi
+  # Actually, we should only be getting the first 100 tags.
+  # However, currently github returns all tags.
+  # See issue: https://github.com/github/docs/issues/3863
+  ENDPOINT="git/matching-refs/tags?per_page=100"
+  RESPONSE_FILTER="^refs/tags/$TAG_FILTER\$"
+  PATH_TO_TAG=".ref"
 
-  AUTH_ARG=""
+  AUTH_ARGS=()
   if [[ -v GITHUB_TOKEN ]]; then
-    AUTH_ARG="-H 'Authorization: Bearer $GITHUB_TOKEN'"
+    AUTH_ARGS+=("-H" "Authorization: $GITHUB_TOKEN")
   fi
 
-  GITHUB_RESPONSE="$(curl -s \
+  GITHUB_RESPONSE="$(curl --silent --show-error --fail-with-body \
     -D "$HEADERS_FILE" \
     --etag-save "$ETAG_FILE" \
     -H "If-None-Match: $ETAG" \
     -H "Accept: application/vnd.github.v3+json" \
-    $AUTH_ARG \
+    "${AUTH_ARGS[@]}" \
     "https://api.github.com/repos/$REPO/$ENDPOINT")"
 
   STATUS_CODE_LINE="$(cat "$HEADERS_FILE" | head -n 1)"
@@ -71,7 +64,10 @@ retrieveLatestVersion() {
 
   FILTERED_TAGS="$(echo "$GITHUB_RESPONSE" | jq -r ".[] | select($PATH_TO_TAG | test(\"$RESPONSE_FILTER\")) | $PATH_TO_TAG")"
 
-  LATEST_VERSION="$(echo "$FILTERED_TAGS" | $LATEST_VERSION_EXTRACTOR -n 1)"
+  LATEST_VERSION="$(
+    echo -n "$FILTERED_TAGS" \
+    | jq -r -R -s "split(\"\n\") | sort_by(split(\"$VERSION_PREFIX\")[1] | split(\".\") | map(tonumber)) | last"
+  )"
 
   if [[ ! -v LATEST_VERSION ]]; then
     echo "Found tags after filtering:"
@@ -85,23 +81,23 @@ retrieveLatestVersion() {
 }
 
 # retrieve version: Google Protobuf
-retrieveLatestVersion "tag" "protocolbuffers/protobuf" "v[0-9]+[.][0-9]+"
+retrieveLatestVersion "tag" "protocolbuffers/protobuf" "v[0-9]+[.][0-9]+" "v"
 export PROTO_VERSION="${LATEST_VERSION#"v"}"
 registerVersion "Protobuf" "PROTO_VERSION"
 
 # retrieve version: go
-retrieveLatestVersion "tag" "golang/go" "go1[.][0-9]+[.][0-9]+"
+retrieveLatestVersion "tag" "golang/go" "go1[.][0-9]+[.][0-9]+" "go"
 GO_VERSION="${LATEST_VERSION#"go"}"
 # GO_VERSION="$(echo "$GO_VERSION" | sed -E "s/\.[0-9]+$//")" # remove patch version
 registerVersion "go" "GO_VERSION"
 
 # retrieve version: goreleaser
-retrieveLatestVersion "tag" "goreleaser/goreleaser" "v1[.][0-9]+[.][0-9]+"
+retrieveLatestVersion "tag" "goreleaser/goreleaser" "v1[.][0-9]+[.][0-9]+" "v"
 export GORELEASER_VERSION="$LATEST_VERSION"
 registerVersion "Goreleaser" "GORELEASER_VERSION"
 
 # retrieve version: protocurl
-retrieveLatestVersion "tag" "qaware/protocurl" "v[0-9]+[.][0-9]+[.][0-9]+"
+retrieveLatestVersion "tag" "qaware/protocurl" "v[0-9]+[.][0-9]+[.][0-9]+" "v"
 export PROTOCURL_RELEASED_VVERSION="$LATEST_VERSION"
 registerVersion "Latest released protoCURL" "PROTOCURL_RELEASED_VVERSION"
 
